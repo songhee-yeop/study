c163ecc7a15250227be88e62d79e8e9c0d4af7ea
hysong <great0417@gmail.com>
2025-02-16 18:19:12 +0900

    feat: 스터디 5장 완료

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+      int age = 36;
+      int year = LocalDate.now().getYear();
+
+      int answer = year -  age + 1;
+        System.out.println(123 / 10);
+    }
+}
